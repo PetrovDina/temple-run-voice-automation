c1b859795646a126aae4a4b7e368b9a13ab416e3
Dina Petrov <dina.petrov@gmail.com>
2022-01-20 10:35:29 +0100

    Comment

--- voice-commands-recognizer.py
+++ voice-commands-recognizer.py
@@ -17,9 +17,6 @@ def callback(recognizer, audio):
     # todo ovde mi pozivamo nasu metodu za kreiranje spektograma i prosledjujemo ga modelu na predikciju
     try:
         print("CALLBACK")
-        # for testing purposes, we're just using the default API key
-        # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
-        # instead of `r.recognize_google(audio)`
         print("Google Speech Recognition thinks you said " + recognizer.recognize_google(audio))
     except sr.UnknownValueError:
         print("Google Speech Recognition could not understand audio")
@@ -36,9 +33,6 @@ with m as source:
 # start listening in the background (note that we don't have to do this inside a `with` statement)
 stop_listening = r.listen_in_background(m, callback, phrase_time_limit=3)
 # `stop_listening` is now a function that, when called, stops background listening
-
-# do some unrelated computations for 5 seconds
-for _ in range(5000): time.sleep(1)  # we're still listening even though the main thread is doing other things
 
 # calling this function requests that the background listener stop listening
 #stop_listening(wait_for_stop=False) # Zakomentarisano: mi necemo da se zasustavi slušanje
