0d218c1c24a83ea1a2bc7321ff2cfe620c9a628b
stasadj <anastasijadjuric@uns.ac.rs>
2022-01-20 15:50:34 +0100

    124x124

--- spectrogram-generator.py
+++ spectrogram-generator.py
@@ -16,11 +16,11 @@ def generate_spectrograms(directory):
         log_mel_spectrogram = librosa.power_to_db(mel_spectrogram)
 
         px = 1 / plt.rcParams['figure.dpi']  # pixel in inches
-        plt.figure(figsize=(42*px, 42*px))
+        plt.figure(figsize=(161*px, 162*px))
         plt.axis('off')
         librosa.display.specshow(log_mel_spectrogram, sr=sr)
         plt.set_cmap('magma')
-        plt.savefig('speech-commands-sgram/{}/{}-{}.png'.format(command, id, command), bbox_inches='tight',
+        plt.savefig('speech-commands-sgram-124x124/{}/{}-{}.png'.format(command, id, command), bbox_inches='tight',
                     transparent=True, pad_inches=0.0)
         id += 1
 
