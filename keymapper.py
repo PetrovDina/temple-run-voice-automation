7c41d622bf5d07ad9b2ea3f78e507cddbd1ab510
stasadj <anastasijadjuric@uns.ac.rs>
2022-01-21 14:39:30 +0100

    keymapper optimization

--- keymapper.py
+++ keymapper.py
@@ -1,29 +1,14 @@
-from pynput import keyboard
 from pynput.keyboard import Key, Controller
-import time
 
 COMMANDS = ['up', 'down', 'left', 'right']
-controller = Controller()
+KEYS = [Key.up, Key.down, Key.left, Key.right]
+CONTROLLER = Controller()
 
 
 def run_command(command_index):
-    detected_command = COMMANDS[command_index]
-    print("PRESSING: " + str(detected_command))
+    CONTROLLER.press(KEYS[command_index])
+    CONTROLLER.release(KEYS[command_index])
+    print(COMMANDS[command_index])
 
-    if detected_command == 'up':
-        controller.press(Key.up)
-        controller.release(Key.up)
 
-    elif detected_command == 'down':
-        controller.press(Key.down)
-        controller.release(Key.down)
 
-    elif detected_command == 'left':
-        controller.press(Key.left)
-        controller.release(Key.left)
-
-    elif detected_command == 'right':
-        controller.press(Key.right)
-        controller.release(Key.right)
-    else:
-        print("UNKNOWN COMMAND KEY PRESS")
--- voice-commands-recognizer-inmemory.py
+++ voice-commands-recognizer-inmemory.py
@@ -16,22 +16,24 @@ def callback(recognizer, audio):
     run_command(predicted_command)
 
 
-print(" ======================== T E M P L E  R U N ========================")
-print("Configuring microphone.. Please wait.")
-r = sr.Recognizer()
-m = sr.Microphone()
+if __name__ == '__main__':
+    print(" ======================== T E M P L E  R U N ========================")
+    print("Configuring microphone.. Please wait.")
+    r = sr.Recognizer()
+    m = sr.Microphone()
 
-with m as source:
-    r.adjust_for_ambient_noise(source)
+    with m as source:
+        r.adjust_for_ambient_noise(source)
 
-stop_listening = r.listen_in_background(m, callback, phrase_time_limit=3)
-print("Microphone listening... Start talking!")
+    stop_listening = r.listen_in_background(m, callback, phrase_time_limit=2)
+    print("Microphone listening... Start talking!")
 
-# calling this function requests that the background listener stop listening
-# stop_listening(wait_for_stop=False) # Commenting this out because we don't want listening to stop
+    # calling this function requests that the background listener stop listening
+    # stop_listening(wait_for_stop=False) # Commenting this out because we don't want listening to stop
 
-# endless loop (because we don't want the main thread to end as it would also kill the listening thread)
-while True:
-    time.sleep(0.1)
+    # endless loop (because we don't want the main thread to end as it would also kill the listening thread)
+    while True:
+        time.sleep(0.1)
+
 
 
