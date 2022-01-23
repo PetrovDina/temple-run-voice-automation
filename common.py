4f14475d8b68162f1b264a73088cf2141bc34123
Dina Petrov <dina.petrov@gmail.com>
2022-01-23 22:14:28 +0100

    changed some folder structure and model names

--- common.py
+++ common.py
@@ -7,10 +7,10 @@ MODE = "SRB"  # "SRB" for serbian language, "ENG" for english language
 if MODE == "SRB":
     COMMANDS = ['skoci', 'dole', 'levo', 'desno']
     DATASET_PATH = 'srb-sgram\\{}\\*.png'
-    MODEL = keras.models.load_model('model-srb-30epochs.h5')
+    MODEL = keras.models.load_model('models\\model-srb-30epochs.h5')
 
 elif MODE == "ENG":
     COMMANDS = ['up', 'down', 'left', 'right']
     DATASET_PATH = 'speech-commands-sgram\\{}\\*.png'
-    MODEL = keras.models.load_model('model-eng-20epochs.h5')
+    MODEL = keras.models.load_model('models\\model-eng-20epochs.h5')
 
