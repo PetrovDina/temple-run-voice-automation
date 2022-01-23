243bd66db8ce36d334af2c7a13d1112fad1bdafe
stasadj <anastasijadjuric@uns.ac.rs>
2022-01-23 19:02:22 +0100

    refactor refactor

--- commands.py
+++ commands.py
@@ -1,3 +1,6 @@
+import tensorflow.keras as keras
+
+
 COMMANDS_ENG = ['up', 'down', 'left', 'right']
 COMMANDS_SRB = ['skoci', 'dole', 'levo', 'desno']
 COMMANDS = COMMANDS_SRB
@@ -7,3 +10,5 @@ DATASET_PATH_SRB = 'srb-sgram\\{}\\*.png'
 
 DATASET_PATH_32_X_32 = DATASET_PATH_SRB
 DATASET_PATH_124_X_124 = 'speech-commands-sgram-124x124\\{}\\*.png'
+
+MODEL = keras.models.load_model('model-32x32-20epoch.h5')
